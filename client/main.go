4019f4cf4327d2b3d24630bb5968bec7ca1c1a88
tianjiajie <tjj2937693919@outlook.com>
2024-02-03 09:50:24 +0800

    海量用户通讯系统

---
+++ client/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+var userId int     //用户的id
+var userPwd string // 用户的密码
+func mian() {
+
+	//接受用户的选择
+	var key int
+	//判断是否还继续显示菜单
+	var loop = true
+
+	for loop {
+		fmt.Println("-------------------欢迎登陆多人聊天系统")
+		fmt.Println("\t\t\t 1 登陆聊天室")
+		fmt.Println("\t\t\t 2 注册用户")
+		fmt.Println("\t\t\t 3 退出系统")
+		fmt.Println("\t\t\t 请选择（1-3）")
+
+		fmt.Scanf("%d\n", &key)
+		switch key {
+		case 1:
+			fmt.Println("登陆聊天室")
+			loop = false
+		case 2:
+			fmt.Println("注册用户")
+			loop = false
+		case 3:
+			fmt.Println("退出系统")
+			loop = false
+		default:
+			fmt.Println("您的输入有误，请重新输入")
+		}
+	}
+
+	//根据用户输入，显示新的提示信息
+	if key == 1 {
+		//说明用户要登陆
+		fmt.Println("请输入用户的id")
+		fmt.Scanf("%d\n", &userId)
+		fmt.Println("请输入用户的密码")
+		fmt.Scanf("%d\n", &userPwd)
+		//先把登录得函数写到另外一个文件
+	}
+
+}
