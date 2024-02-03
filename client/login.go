a9ebe82845a7a8297cffe64f171cc68941e0bbe4
tianjiajie <tjj2937693919@outlook.com>
2024-02-03 11:01:55 +0800

    登录界面

---
+++ client/login.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// 写一个函数，完成登录校验
+func login(userId int, userPwd string) (err error) {
+	fmt.Printf("userId = %d userPwd = %s\n", userId, userPwd)
+	return nil
+
+}
--- client/main.go
+++ client/main.go
@@ -41,7 +41,16 @@ func mian() {
 		fmt.Scanf("%d\n", &userId)
 		fmt.Println("请输入用户的密码")
 		fmt.Scanf("%d\n", &userPwd)
-		//先把登录得函数写到另外一个文件
+		//先把登录得函数写到另外一个文件,比如login.go
+		err := login(userId, userPwd)
+		if err != nil {
+			fmt.Println("登陆失败")
+		} else {
+			fmt.Println("登陆成功")
+		}
+
+	} else if key == 2 {
+		fmt.Println("进行用户注册")
 	}
 
 }
